449f3a8bdde39f5cbfeca11b935115c693a6ec7d
arizaseba <sebaa.ariza@gmail.com>
2022-07-05 16:34:07 -0300

    Desafío 2

---
+++ js/script.js
@@ -0,0 +1,57 @@
+let Username = null;
+let Password = null;
+
+function CrearCuenta()
+{
+    if (Username == null && Password == null)
+    {
+        if (document.getElementById("username").value != "" && document.getElementById("password").value != "")
+        {
+            Username = document.getElementById("username").value;
+            Password = document.getElementById("password").value;
+            Object.freeze(Username);
+            Object.freeze(Password);
+            alert("Cuenta creada exitosamente");
+            document.getElementById("username").value = "";
+            document.getElementById("password").value = "";
+        }
+        else 
+        {
+            alert("Complete los campos");
+        }
+    }
+    else
+    {
+        alert(`Ya se ha creado la cuenta ${Username}`);
+    }
+}
+
+let intentos = 3; // cantidad de intentos para iniciar sesion
+function ValidarSesion()
+{
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+    if (user == Username && pass == Password)
+    {
+        alert ("¡Sesión iniciada!");
+        return false;
+    }
+    else if (Username == null && Password == null)
+    {
+        alert ("No existe ninguna cuenta creada");
+    }
+    else
+    {
+        intentos --;
+        alert("Tienes " + intentos + " intentos");
+        if (intentos == 0)
+        {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
+
+
