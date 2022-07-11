849bd7cb55922e83d4dbba2225bce004b8b19366
arizaseba <sebaa.ariza@gmail.com>
2022-07-11 20:43:55 -0300

    desafio1

--- js/script.js
+++ js/script.js
@@ -1,59 +1,31 @@
-let Username = null;
-let Password = null;
+let user = "";
+let pass = "";
+let validarUser = false;
+let validarPass = false;
 
-function CrearCuenta()
-{
-    if (Username == null && Password == null)
-    {
-        if (document.getElementById("username").value != "" && document.getElementById("password").value != "")
-        {
-            Username = document.getElementById("username").value;
-            Password = document.getElementById("password").value;
-            Object.freeze(Username);
-            Object.freeze(Password);
-            alert("¡Cuenta creada exitosamente!");
-            document.getElementById("username").value = "";
-            document.getElementById("password").value = "";
-            document.getElementById("create").disabled = true;
+user = prompt("Registra tu nombre de Usuario:").toLowerCase();
+pass = prompt("Registra una contraseña:");
+
+do {
+    if (!validarUser) {
+        if (prompt("Usuario:").toLowerCase() === user) {
+            validarUser = true;
         }
-        else
-        {
-            alert("Complete los campos");
+        else {
+            alert("⛔ Usuario incorrecto. Intente nuevamente.");
         }
     }
-    else
-    {
-        alert(`Ya se ha creado la cuenta ${Username}`);
-    }
-}
+} while (!validarUser);
 
-let intentos = 3; // cantidad de intentos para iniciar sesion
-function ValidarSesion()
-{
-    let user = document.getElementById("username").value;
-    let pass = document.getElementById("password").value;
-    if (user == Username && pass == Password)
-    {
-        alert("¡Sesión iniciada!");
-        //window.location = "#"; //dirige a una pagina despues de iniciar sesión
-        return false;
-    }
-    else if (Username == null && Password == null)
-    {
-        alert("No existe ninguna cuenta creada");
-    }
-    else
-    {
-        intentos --;
-        if (intentos == 0)
-        {
-            document.getElementById("username").disabled = true;
-            document.getElementById("password").disabled = true;
-            document.getElementById("submit").disabled = true;
-            return false;
+do{
+    if (!validarPass) {
+        if (prompt("Contraseña:") === user) {
+            validarPass = true;
         }
-        alert("Tienes " + intentos + " intentos");
+        else {
+            alert("⛔ Contraseña incorrecta. Intente nuevamente.");
+        }
     }
-}
+} while (!validarPass);
 
-
+alert("✅ ¡Sesion iniciada!"); 
