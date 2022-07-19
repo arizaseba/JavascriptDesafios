bda6280dfb6f065a5407ea3c4e22152292160073
arizaseba <sebaa.ariza@gmail.com>
2022-07-19 14:25:35 -0300

    Interactuar con HTML

--- js/script.js
+++ js/script.js
@@ -1,59 +1,121 @@
-let Username = null;
-let Password = null;
+const Usuarios = []
+
+class Usuario {
+    constructor (username, password) {
+        this.username = username
+        this.password = password
+    }
+}
+
+document.getElementById("createForm").style.display = "none"
+document.getElementById("labelResult").style.display = "none"
+
+function MessageBox(mensaje, error) 
+{
+    if (error) 
+    {
+        document.getElementById("labelResult").style.borderColor = "red"
+        document.getElementById("labelResult").style.color = "red"
+    }
+    else
+    {
+        document.getElementById("labelResult").style.borderColor = "green"
+        document.getElementById("labelResult").style.color = "green"
+    }
+    document.getElementById("labelResult").style.display = "initial"
+    document.getElementById("labelResult").textContent = mensaje
+}
 
 function CrearCuenta()
 {
-    if (Username == null && Password == null)
+    let user = document.getElementById("createUsername").value
+    let pass = document.getElementById("createPassword").value
+ 
+    if (user == "" || pass == "")
     {
-        if (document.getElementById("username").value != "" && document.getElementById("password").value != "")
+        MessageBox("Complete los campos", true)
+        return
+    }
+
+    if (!Usuarios.some(u => u.username == user))
+    {
+        Usuarios.push(new Usuario(user, pass));
+
+        MessageBox("¡Cuenta creada exitosamente!", false)
+        document.getElementById("createUsername").value = ""
+        document.getElementById("createPassword").value = ""
+
+        // mostrar array de usuarios
+        console.log(Usuarios)
+    }
+    else
+    {
+        MessageBox(`Ya se ha creado la cuenta ${user}`, true)
+    }
+}
+
+function ValidarSesion()
+{
+    let user = document.getElementById("loginUsername").value
+    let pass = document.getElementById("loginPassword").value
+    const usu = Usuarios.find( u => u.username == user)
+
+    if (user == "" || pass == "")
+    {
+        MessageBox("Complete los campos", true)
+        return
+    }
+    if (usu != null)
+    {
+        if (user == user && pass == usu.password)
         {
-            Username = document.getElementById("username").value;
-            Password = document.getElementById("password").value;
-            Object.freeze(Username);
-            Object.freeze(Password);
-            alert("¡Cuenta creada exitosamente!");
-            document.getElementById("username").value = "";
-            document.getElementById("password").value = "";
-            document.getElementById("create").disabled = true;
+            MessageBox("¡Sesión iniciada!", false)
         }
         else
         {
-            alert("Complete los campos");
+            MessageBox("Usuario o contraseña incorrectos.Intente nuevamente.", true)
+            document.getElementById("loginUsername").value = ""
+            document.getElementById("loginPassword").value = ""
         }
     }
     else
     {
-        alert(`Ya se ha creado la cuenta ${Username}`);
-    }
-}
-
-let intentos = 3; // cantidad de intentos para iniciar sesion
-function ValidarSesion()
-{
-    let user = document.getElementById("username").value;
-    let pass = document.getElementById("password").value;
-    if (user == Username && pass == Password)
-    {
-        alert("¡Sesión iniciada!");
-        //window.location = "#"; //dirige a una pagina despues de iniciar sesión
-        return false;
-    }
-    else if (Username == null && Password == null)
-    {
-        alert("No existe ninguna cuenta creada");
-    }
-    else
-    {
-        intentos --;
-        if (intentos == 0)
-        {
-            document.getElementById("username").disabled = true;
-            document.getElementById("password").disabled = true;
-            document.getElementById("submit").disabled = true;
-            return false;
-        }
-        alert("Tienes " + intentos + " intentos");
+        MessageBox(`No existe el usuario ${user}`, true)
     }
 }
 
 
+const loginButton = document.getElementById("loginButton")
+loginButton.addEventListener(("click"), event => {
+    event.preventDefault()
+    document.getElementById("labelResult").style.display = "none"
+    ValidarSesion()
+})
+
+const createButton = document.getElementById("createButton")
+createButton.addEventListener(("click"), event => {
+    event.preventDefault()
+    document.getElementById("labelResult").style.display = "none"
+    CrearCuenta()
+})
+
+const formCreate = document.getElementById("labelLogin")
+formCreate.addEventListener(("click"), event => {
+    if (document.getElementById("createForm").style.display == "none") {
+        event.preventDefault()
+        document.getElementById("loginForm").style.display = "none"
+        document.getElementById("createForm").style.display = "initial"
+        document.getElementById("labelLogin").textContent = "Iniciar Sesión"
+    }
+    else {
+        event.preventDefault()
+        document.getElementById("loginForm").style.display = "initial"
+        document.getElementById("createForm").style.display = "none"
+        document.getElementById("labelLogin").textContent = "Crear Cuenta"
+    }
+    document.getElementById("loginUsername").value = ""
+    document.getElementById("loginPassword").value = ""
+    document.getElementById("createUsername").value = ""
+    document.getElementById("createPassword").value = ""
+    document.getElementById("labelResult").style.display = "none"
+})
